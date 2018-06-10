9902f11ef4bd598d880dd32ad640456734ec604e
IRON56 <40119494+IRON56@users.noreply.github.com>
2018-06-10 10:18:42 +0800

    Update SimpleStorage.sol

--- contract/SimpleStorage.sol
+++ contract/SimpleStorage.sol
@@ -1,11 +1,23 @@
 contract SimpleStorage {
-    string storedData;
+    string storedData1;
+    string storedData2;
+    string storedData3;
+    
 
     function set(string x) {
-        storedData = x;
+        if (!storedData1)
+            storedData1=x;
+        else if (!storedData2)
+            storedData2=x;
+        else if (!storedData3)
+            storedData3=x;
     }
 
     function get() constant returns (string x) {
+        var storedData= "";
+        storedData+=storedData1;
+        storedData+=storedData2;
+        storedData+=storedData3;
         return storedData;
     }
 }
